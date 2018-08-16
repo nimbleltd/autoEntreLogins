c4cdbbdb10643d2246b4de34ce2d6e3e7444179a
Paul Campbell <nimbleltd@gmail.com>
2018-08-16 07:13:38 -0500

    added notes file

---
+++ NOTES.py
@@ -0,0 +1,9 @@
+# NOTES
+
+# attributes you can retrieve from an element
+# for element in self.driver.find_elements_by_tag_name('img'):
+#        print element.text
+#        print element.tag_name
+#        print element.parent
+#        print element.location
+#        print element.size
